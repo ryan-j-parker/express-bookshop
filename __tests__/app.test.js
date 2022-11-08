a8dda29ef4d8fe13659a74316b876d23cfd2ae91
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-08 13:55:25 -0800

    get all books test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,6 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('express');
-// const request = require('supertest');
+const request = require('supertest');
 const app = require('../lib/app');
 const { books } = require('../lib/books-data');
 
@@ -14,7 +13,7 @@ describe('books routes', () => {
     const res = await request(app).get('/books');
 
     const expected = books.map((book) => {
-      return { id: book.id, title: book.title, author: book.author };
+      return { id: book.id, title: book.title, author: book.author, published: book.published };
     });
 
     expect(res.body).toEqual(expected);
